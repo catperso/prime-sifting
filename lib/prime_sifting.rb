2cac209abd0955872e956e0552fc237e8f1abe29
Frank-Proulx <frankproulx7@gmail.com>
2021-12-09 15:22:40 -0800

    complete tests and methods, working
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -2,7 +2,7 @@ class PrimeSifter
   attr_writer(:number)
 
   def initialize(number)
-    @number = number
+    @number = number.to_i
   end
   
   def array_maker
@@ -17,8 +17,12 @@ class PrimeSifter
 
   def reducer
     working_array = array_maker()
-    prime = 2
-    working_array.reject! {|element| element % prime == 0}
+    prime_index = 0
+    until prime_index == (working_array.length - 1)
+      prime = working_array[prime_index]
+      working_array.reject! {|element| (element % prime == 0) && (element != prime)}
+      prime_index += 1
+    end
     working_array
   end
 
